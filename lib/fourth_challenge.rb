11f36b922d473cef40290e7f6a62adf49f0a133b
Hoon Lee <yl.hoony@gmail.com>
2017-11-27 20:39:38 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,5 +1,5 @@
 def fourth_challenge
-  epic_tragedy = {
+  epic_tragedy = { 
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
